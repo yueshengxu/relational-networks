3b2be17ae8dda7fd2792cc0cef90b75a572d4c55
yueshengxu <xuyuesheng324@gmail.com>
2022-12-01 16:43:30 -0500

    update RN
    update RN

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 from torch.autograd import Variable
 
-from model import RN, CNN_MLP
+from model import RN2, RN, CNN_MLP
 
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch Relational-Network sort-of-CLVR Example')
-parser.add_argument('--model', type=str, choices=['RN', 'CNN_MLP'], default='RN', 
+parser.add_argument('--model', type=str, choices=['RN2','RN', 'CNN_MLP'], default='RN2', 
                     help='resume from model stored')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
@@ -50,6 +50,8 @@ summary_writer = SummaryWriter()
 
 if args.model=='CNN_MLP': 
   model = CNN_MLP(args)
+elif args.model=='RN2':
+  model = RN2(args)
 else:
   model = RN(args)
   
@@ -86,34 +88,30 @@ def cvt_data_axis(data):
     return (img,qst,ans)
 
     
-def train(epoch, ternary, rel, norel):
+def train(epoch, rel, norel):
     model.train()
 
     if not len(rel[0]) == len(norel[0]):
         print('Not equal length for relation dataset and non-relation dataset.')
         return
-    
-    random.shuffle(ternary)
+
     random.shuffle(rel)
     random.shuffle(norel)
 
-    ternary = cvt_data_axis(ternary)
+
     rel = cvt_data_axis(rel)
     norel = cvt_data_axis(norel)
 
-    acc_ternary = []
+
     acc_rels = []
     acc_norels = []
 
-    l_ternary = []
+
     l_binary = []
     l_unary = []
 
     for batch_idx in range(len(rel[0]) // bs):
-        tensor_data(ternary, batch_idx)
-        accuracy_ternary, loss_ternary = model.train_(input_img, input_qst, label)
-        acc_ternary.append(accuracy_ternary.item())
-        l_ternary.append(loss_ternary.item())
+
 
         tensor_data(rel, batch_idx)
         accuracy_rel, loss_binary = model.train_(input_img, input_qst, label)
@@ -127,61 +125,52 @@ def train(epoch, ternary, rel, norel):
 
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)] '
-                  'Ternary accuracy: {:.0f}% | Relations accuracy: {:.0f}% | Non-relations accuracy: {:.0f}%'.format(
+                  'Relations accuracy: {:.0f}% | Non-relations accuracy: {:.0f}%'.format(
                    epoch,
                    batch_idx * bs * 2,
                    len(rel[0]) * 2,
                    100. * batch_idx * bs / len(rel[0]),
-                   accuracy_ternary,
                    accuracy_rel,
                    accuracy_norel))
         
-    avg_acc_ternary = sum(acc_ternary) / len(acc_ternary)
     avg_acc_binary = sum(acc_rels) / len(acc_rels)
     avg_acc_unary = sum(acc_norels) / len(acc_norels)
 
     summary_writer.add_scalars('Accuracy/train', {
-        'ternary': avg_acc_ternary,
         'binary': avg_acc_binary,
         'unary': avg_acc_unary
     }, epoch)
 
-    avg_loss_ternary = sum(l_ternary) / len(l_ternary)
     avg_loss_binary = sum(l_binary) / len(l_binary)
     avg_loss_unary = sum(l_unary) / len(l_unary)
 
     summary_writer.add_scalars('Loss/train', {
-        'ternary': avg_loss_ternary,
         'binary': avg_loss_binary,
         'unary': avg_loss_unary
     }, epoch)
 
     # return average accuracy
-    return avg_acc_ternary, avg_acc_binary, avg_acc_unary
+    return avg_acc_binary, avg_acc_unary
 
-def test(epoch, ternary, rel, norel):
+def test(epoch, rel, norel):
     model.eval()
     if not len(rel[0]) == len(norel[0]):
         print('Not equal length for relation dataset and non-relation dataset.')
         return
     
-    ternary = cvt_data_axis(ternary)
+
     rel = cvt_data_axis(rel)
     norel = cvt_data_axis(norel)
 
-    accuracy_ternary = []
+
     accuracy_rels = []
     accuracy_norels = []
 
-    loss_ternary = []
+
     loss_binary = []
     loss_unary = []
 
     for batch_idx in range(len(rel[0]) // bs):
-        tensor_data(ternary, batch_idx)
-        acc_ter, l_ter = model.test_(input_img, input_qst, label)
-        accuracy_ternary.append(acc_ter.item())
-        loss_ternary.append(l_ter.item())
 
         tensor_data(rel, batch_idx)
         acc_bin, l_bin = model.test_(input_img, input_qst, label)
@@ -193,29 +182,27 @@ def test(epoch, ternary, rel, norel):
         accuracy_norels.append(acc_un.item())
         loss_unary.append(l_un.item())
 
-    accuracy_ternary = sum(accuracy_ternary) / len(accuracy_ternary)
+
     accuracy_rel = sum(accuracy_rels) / len(accuracy_rels)
     accuracy_norel = sum(accuracy_norels) / len(accuracy_norels)
-    print('\n Test set: Ternary accuracy: {:.0f}% Binary accuracy: {:.0f}% | Unary accuracy: {:.0f}%\n'.format(
-        accuracy_ternary, accuracy_rel, accuracy_norel))
+    print('\n Test set:  Binary accuracy: {:.0f}% | Unary accuracy: {:.0f}%\n'.format(
+         accuracy_rel, accuracy_norel))
 
     summary_writer.add_scalars('Accuracy/test', {
-        'ternary': accuracy_ternary,
         'binary': accuracy_rel,
         'unary': accuracy_norel
     }, epoch)
 
-    loss_ternary = sum(loss_ternary) / len(loss_ternary)
+
     loss_binary = sum(loss_binary) / len(loss_binary)
     loss_unary = sum(loss_unary) / len(loss_unary)
 
     summary_writer.add_scalars('Loss/test', {
-        'ternary': loss_ternary,
         'binary': loss_binary,
         'unary': loss_unary
     }, epoch)
 
-    return accuracy_ternary, accuracy_rel, accuracy_norel
+    return accuracy_rel, accuracy_norel
 
     
 def load_data():
@@ -224,8 +211,7 @@ def load_data():
     filename = os.path.join(dirs,'sort-of-clevr.pickle')
     with open(filename, 'rb') as f:
       train_datasets, test_datasets = pickle.load(f)
-    ternary_train = []
-    ternary_test = []
+
     rel_train = []
     rel_test = []
     norel_train = []
@@ -234,8 +220,7 @@ def load_data():
 
     for img, ternary, relations, norelations in train_datasets:
         img = np.swapaxes(img, 0, 2)
-        for qst, ans in zip(ternary[0], ternary[1]):
-            ternary_train.append((img,qst,ans))
+
         for qst,ans in zip(relations[0], relations[1]):
             rel_train.append((img,qst,ans))
         for qst,ans in zip(norelations[0], norelations[1]):
@@ -243,18 +228,17 @@ def load_data():
 
     for img, ternary, relations, norelations in test_datasets:
         img = np.swapaxes(img, 0, 2)
-        for qst, ans in zip(ternary[0], ternary[1]):
-            ternary_test.append((img, qst, ans))
+
         for qst,ans in zip(relations[0], relations[1]):
             rel_test.append((img,qst,ans))
         for qst,ans in zip(norelations[0], norelations[1]):
             norel_test.append((img,qst,ans))
     
-    return (ternary_train, ternary_test, rel_train, rel_test, norel_train, norel_test)
-    
 
-ternary_train, ternary_test, rel_train, rel_test, norel_train, norel_test = load_data()
+    return (rel_train, rel_test, norel_train, norel_test)
 
+
+rel_train, rel_test, norel_train, norel_test = load_data()
 try:
     os.makedirs(model_dirs)
 except:
@@ -268,19 +252,23 @@ if args.resume:
         model.load_state_dict(checkpoint)
         print('==> loaded checkpoint {}'.format(filename))
 
+# train length = 98000
+
+
+
 with open(f'./{args.model}_{args.seed}_log.csv', 'w') as log_file:
     csv_writer = csv.writer(log_file, delimiter=',')
-    csv_writer.writerow(['epoch', 'train_acc_ternary', 'train_acc_rel',
-                     'train_acc_norel', 'train_acc_ternary', 'test_acc_rel', 'test_acc_norel'])
+    csv_writer.writerow(['epoch', 'train_acc_rel',
+                     'train_acc_norel', 'test_acc_rel', 'test_acc_norel'])
 
     print(f"Training {args.model} {f'({args.relation_type})' if args.model == 'RN' else ''} model...")
 
     for epoch in range(1, args.epochs + 1):
-        train_acc_ternary, train_acc_binary, train_acc_unary = train(
-            epoch, ternary_train, rel_train, norel_train)
-        test_acc_ternary, test_acc_binary, test_acc_unary = test(
-            epoch, ternary_test, rel_test, norel_test)
+        train_acc_binary, train_acc_unary = train(
+            epoch, rel_train, norel_train)
+        test_acc_binary, test_acc_unary = test(
+            epoch, rel_test, norel_test)
 
-        csv_writer.writerow([epoch, train_acc_ternary, train_acc_binary,
-                         train_acc_unary, test_acc_ternary, test_acc_binary, test_acc_unary])
-        model.save_model(epoch)
+        csv_writer.writerow([epoch, train_acc_binary,
+                         train_acc_unary, test_acc_binary, test_acc_unary])
+        #model.save_model(epoch)
